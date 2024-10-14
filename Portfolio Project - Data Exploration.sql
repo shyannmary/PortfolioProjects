da50d6f0fd144f59111552078109450889324d4b
shyannmary <annn.kalugina@gmail.com>
2024-10-14 14:59:11 +0300

    Update Portfolio Project - Data Exploration.sql

--- Portfolio Project - Data Exploration.sql
+++ Portfolio Project - Data Exploration.sql
@@ -126,13 +126,10 @@ RollingPeopleVaccinated numeric
 Insert into #PercentPopulationVaccinated
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
 From PortfolioProject..CovidDeaths dea
 Join PortfolioProject..CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
---where dea.continent is not null 
---order by 2,3
 
 Select *, (RollingPeopleVaccinated/Population)*100
 From #PercentPopulationVaccinated
